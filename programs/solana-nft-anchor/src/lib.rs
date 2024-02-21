baa91151acbd9f534b551d4b0513ef3bb8cbf78f
Abhinaw Kumar <abhinawgumla@gmail.com>
2024-02-22 00:12:05 +0530

    first commit

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("HvVaQV8fkfQuH3UdRxJsMJwn3JyM2Kzvkwe87krK1bmq");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
